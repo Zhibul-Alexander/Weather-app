500f90a01d5943d35fb90365331a1f7f53501fd6
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-04-12 11:18:17 +0300

    init(main): add new file

---
+++ src/components/common/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export const Loader = () => {
+    return (
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    )
+}
---
+++ src/constants/loadStatuses.js
@@ -0,0 +1,6 @@
+export const LOAD_STATUSES = {
+  LOADING: "LOADING",
+  LOADED: "LOADED",
+  ERROR: "ERROR",
+  UNKNOWN: "UNKNOWN",
+};
